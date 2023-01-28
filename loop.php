dde9e932dca648090dea455dcf781d7c2d740429
nasimqureshi <qureshi.nasim@yahoo.com>
2023-01-28 13:52:23 -0600

    commit

--- loop.php
+++ loop.php
@@ -13,14 +13,27 @@
 // }
 //  echo "Loops ends";
 
-$i = 0;
-while($i < 10 ){
-    echo $i."<br />";
+// $i = 0;
+// while($i < 10 ){
+//     echo $i."<br />";
     
-    $i++;
+//     $i++;
+// }
+
+// For Each Loop
+$arr = [
+    "Nasim" => 69,
+    "Qamar" => 66,
+    "Samreen" => 42,
+    "Hira" => 39,
+];
+// echo "<pre>";
+// print_r($arr);
+// echo "</pre>";
+
+foreach ($arr as $name => $age){
+    echo "The $name year old is $age<br/>";
 }
-
-
 
 
 ?>
