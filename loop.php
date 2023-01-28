3e02e1941715b848b71c47f5655e7500522f2c2a
nasimqureshi <qureshi.nasim@yahoo.com>
2023-01-28 13:32:19 -0600

    commit

---
+++ loop.php
@@ -0,0 +1,21 @@
+<?php
+// // 10 times Nasim Qureshi
+// $i = 0;
+
+// while($i < 10){
+//     echo "Nasim Qureshi<br/>";
+//     $i++;
+// }
+// echo "Loops ends"
+
+for($i = 100; $i < 0; $i--){
+    echo $i."<br />";
+}
+ echo "Loops ends";
+
+
+
+
+
+
+?>
